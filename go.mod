8804ccc99f6c59ece6216b935107a2ffb53bdaf9
thenewlebowski <coltonn@cryptnode.tech>
2022-12-02 21:54:49 -0700

    adding more socket support

--- go.mod
+++ go.mod
@@ -13,7 +13,9 @@ require (
 	github.com/cryptnode-software/grpc v0.0.0-20221123231124-391714d8a847
 	github.com/golang/protobuf v1.5.2
 	github.com/google/uuid v1.3.0
+	github.com/googollee/go-socket.io v1.6.2
 	github.com/improbable-eng/grpc-web v0.15.0
+	github.com/kr/pretty v0.1.0
 	go.buf.build/grpc/go/thenewlebowski/hermes v1.4.2
 	gorm.io/driver/mysql v1.3.6
 	gorm.io/gorm v1.23.10
@@ -25,12 +27,12 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gofrs/uuid v4.0.0+incompatible // indirect
 	github.com/gomodule/redigo v1.8.4 // indirect
-	github.com/googollee/go-socket.io v1.6.2 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.11.7 // indirect
+	github.com/kr/text v0.1.0 // indirect
 	github.com/rs/cors v1.7.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
--- services/socket/service.go
+++ services/socket/service.go
@@ -2,18 +2,19 @@ package socket
 
 import (
 	"fmt"
+	"net/http"
 
 	commons "github.com/cryptnode-software/commons/pkg"
 	db "github.com/cryptnode-software/hermes/gorm"
 	socketio "github.com/googollee/go-socket.io"
+	"github.com/googollee/go-socket.io/engineio"
+	"github.com/googollee/go-socket.io/engineio/transport"
+	"github.com/googollee/go-socket.io/engineio/transport/polling"
+	"github.com/googollee/go-socket.io/engineio/transport/websocket"
 	"gorm.io/gorm"
 )
 
 type sockets map[string][]Socket
-
-var (
-	open = sockets{}
-)
 
 type Service struct {
 	db     *gorm.DB
@@ -35,18 +36,36 @@ func NewService() (service *Service, err error) {
 		return
 	}
 
-	service.server = socketio.NewServer(nil)
+	service.server =
+		socketio.NewServer(&engineio.Options{
+			Transports: []transport.Transport{
+				&polling.Transport{
+					CheckOrigin: func(r *http.Request) bool {
+						return true
+					},
+				},
+				&websocket.Transport{
+					CheckOrigin: func(r *http.Request) bool {
+						return true
+					},
+				},
+			},
+		})
 
 	service.server.OnConnect("/", func(s socketio.Conn) error {
 		s.SetContext("")
 		fmt.Println("connected:", s.ID())
+		s.Join("chat")
 		return nil
 	})
 
-	service.server.OnEvent("/", "msg", func(s socketio.Conn) {
-		msg := s.Context().(string)
-		s.Emit("msg", msg)
+	service.server.OnEvent("/", "message", func(s socketio.Conn, message string) {
+		service.server.BroadcastToRoom("", "chat", "message", message)
 		return
+	})
+
+	service.server.OnEvent("/", "connect", func(s socketio.Conn, user string) {
+		service.server.BroadcastToRoom("", "chat", "connect", user)
 	})
 
 	return
